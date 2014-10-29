0e23a856004c9cc7957e1adebd7e438d6b875281
Valentin Bryukhanov <bvv.mag@gmail.com>
2014-10-29 16:44:09 +0300

    referee

--- verification/referee.py
+++ verification/referee.py
@@ -4,6 +4,16 @@ from checkio.referees.io import CheckiOReferee
 
 from tests import TESTS
 
+cover = """def cover(f, data):
+    return bool(f(tuple(tuple(d) for d in data[0]), tuple(data[1])))
+"""
+
 api.add_listener(
     ON_CONNECT,
-    CheckiOReferee(tests=TESTS).on_ready)
+    CheckiOReferee(
+        tests=TESTS,
+        cover_code={
+            'python-27': cover,  # or None
+            'python-3': cover
+        },
+        function_name="is_inside").on_ready)
