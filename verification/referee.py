9581b75cf7e868c68690143c736114f443580d42
Oleksandr Zozulia <freemanlex@gmail.com>
2023-07-23 15:48:33 +0300

    next api

--- verification/referee.py
+++ verification/referee.py
@@ -1,23 +1,23 @@
 from checkio.signals import ON_CONNECT
 from checkio import api
-from checkio.referees.io import CheckiOReferee
-from checkio.referees import cover_codes
+from checkio.referees.io_template import CheckiOReferee
+# from checkio.referees.checkers import to_list
 
 from tests import TESTS
-
-cover = """def cover(f, data):
-    return bool(f(tuple(tuple(d) for d in data[0]), tuple(data[1])))
-"""
 
 api.add_listener(
     ON_CONNECT,
     CheckiOReferee(
         tests=TESTS,
+        # checker=to_list,
         function_name={
             "python": "is_inside",
             "js": "isInside"
         },
         cover_code={
-            'python-3': cover,
-            'js-node': cover_codes.js_unwrap_args
-        }).on_ready)
+            "python-3": {},
+            "js-node": {
+                # "dateForZeros": True,
+            }
+        }
+    ).on_ready)
